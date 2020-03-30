d314a9da3f4b902aace7c4b75794045eed4272b7
Noah Doughty <noahdoughty@gmail.com>
2020-03-29 21:20:00 -0400

    Worked through video 29

--- src/org/wcci/Main.java
+++ src/org/wcci/Main.java
@@ -4,59 +4,85 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int myValue = 10000;
+//        int myValue = 10000;
+//
+//        int myMinIntValue = Integer.MIN_VALUE;
+//        int myMaxIntValue = Integer.MAX_VALUE;
+//        System.out.println("Integer Minimum Value = " + myMinIntValue);
+//        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+//        System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
+//        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+//
+//        int myMaxIntTest = 2147483647;
+//
+//        byte myMinByteValue = Byte.MIN_VALUE;
+//        byte myMaxByteValue = Byte.MAX_VALUE;
+//        System.out.println("Byte Minimum Value = " + myMinByteValue);
+//        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+//
+//        short myMinShortValue = Short.MIN_VALUE;
+//        short myMaxShortValue = Short.MAX_VALUE;
+//        System.out.println("Short Minimum Value = " + myMinShortValue);
+//        System.out.println("Short Maximum Value = " + myMaxShortValue);
+//
+//        long myLongValue = 100L;
+//        long myMinLongValue = Long.MIN_VALUE;
+//        long myMaxLongValue = Long.MAX_VALUE;
+//        System.out.println("Long Minimum Value = " + myMinLongValue);
+//        System.out.println("Long Maximum Value = " + myMaxLongValue);
+//        long bigLongLiteralValue = 2_147_483_647_234L;
+//        System.out.println(bigLongLiteralValue);
+//
+//        short bigShortLiteralValue = 32767;
+//
+//        int myTotal = (myMinIntValue / 2);
+//
+//        byte myNewByteValue = (byte) (myMinByteValue / 2);
+//
+//        short myNewShortValue = (short) (myMinShortValue / 2);
+//
+////        Primitive Type Challenge
+//        byte myByte = 40;
+//        short myShort = 17893;
+//        int myInt = 7365869;
+//        long myLong = 50000L + 10L * (myByte + myShort + myInt); //Add capital L to indicate long number
+//        System.out.println(myLong);
+//
+//        short shortTotal = (short) (1000 + 10 *
+//                (myByte + myShort + myInt));
+//
+//        System.out.println(shortTotal);
+//
+//        float myMinFloatValue = Float.MIN_VALUE;
+//        float myMaxFloatValue = Float.MAX_VALUE;
+//        System.out.println("Float minimum value = " + myMinFloatValue);
+//        System.out.println("Float maximum value = " + myMaxFloatValue);
+//
+//        double myMinDoubleValue = Double.MIN_VALUE;
+//        double myMaxDoubleValue = Double.MAX_VALUE;
+//        System.out.println("Double minimum value = " + myMinDoubleValue);
+//        System.out.println("Double maximum value = " + myMaxDoubleValue);
+//
+//        int myIntValue = 5 / 3;
+//        float myFloatValue = 5f / 3f;   //add f to indicate float number
+//        double myDoubleValue = 5.00 / 3.00;  //add d to indicate double number
+//        System.out.println("My Int Value = " + myIntValue);
+//        System.out.println("My Float Value = " + myFloatValue);
+//        System.out.println("My Double Value = " + myDoubleValue);
+//
+////        Challenge- Convert a given number of pounds to kilograms
+//
+//        double myPounds = 200d;
+//        double kiloPerPound = 0.45359237d;
+//        double myPoundsConverted = (myPounds * kiloPerPound);
+//        System.out.println("Converted Kilograms = " + myPoundsConverted);
+//
+//        double pi = 3.1415927d;
+//        double anotherNumber = 3_000_000.4_567_890d;
+//        System.out.println(pi);
+//        System.out.println(anotherNumber);
 
-        int myMinIntValue = Integer.MIN_VALUE;
-        int myMaxIntValue = Integer.MAX_VALUE;
-        System.out.println("Integer Minimum Value = " + myMinIntValue);
-        System.out.println("Integer Maximum Value = " + myMaxIntValue);
-        System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
-        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
-
-        int myMaxIntTest = 2147483647;
-
-        byte myMinByteValue = Byte.MIN_VALUE;
-        byte myMaxByteValue = Byte.MAX_VALUE;
-        System.out.println("Byte Minimum Value = " + myMinByteValue);
-        System.out.println("Byte Maximum Value = " + myMaxByteValue);
-
-        short myMinShortValue = Short.MIN_VALUE;
-        short myMaxShortValue = Short.MAX_VALUE;
-        System.out.println("Short Minimum Value = " + myMinShortValue);
-        System.out.println("Short Maximum Value = " + myMaxShortValue);
-
-        long myLongValue = 100L;
-        long myMinLongValue = Long.MIN_VALUE;
-        long myMaxLongValue = Long.MAX_VALUE;
-        System.out.println("Long Minimum Value = " + myMinLongValue);
-        System.out.println("Long Maximum Value = " + myMaxLongValue);
-        long bigLongLiteralValue = 2_147_483_647_234L;
-        System.out.println(bigLongLiteralValue);
-
-        short bigShortLiteralValue = 32767;
-
-        int myTotal = (myMinIntValue / 2);
-
-        byte myNewByteValue = (byte) (myMinByteValue / 2);
-
-        short myNewShortValue = (short) (myMinShortValue / 2);
-
-//        Primitive Type Challenge
-        byte myByte = 40;
-        short myShort = 17893;
-        int myInt = 7365869;
-        long myLong = 50000L + 10L * (myByte + myShort + myInt);
-        System.out.println(myLong);
-
-        short shortTotal = (short) (1000 + 10 *
-                (myByte + myShort + myInt));
-
-        System.out.println(shortTotal);
-
-
-
-
-
+        char myChar = 'D';
 
 
     }
